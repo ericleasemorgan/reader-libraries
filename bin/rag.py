bdb796c55be2bbdd728c87dc8a76ba9733c8fb26
Eric Lease Morgan <eric_morgan@infomotions.com>
2025-10-26 14:50:07 -0400

    Tweaking

--- bin/rag.py
+++ bin/rag.py
@@ -9,15 +9,15 @@
 
 
 # configure
-KEY             = 'code4lib'
-NAME            = 'Code4Lib Journal'
+KEY             = 'dlib'
+NAME            = 'DLib Magazine'
 QUERY           = 'library catalogs discovery systems'
 DEPTH           = 32
 LLM             = 'deepseek-v3.1:671b-cloud'
 PROMPTSUMMARIZE = 'Summarize: %s'
 PROMPTELABORATE = 'Answer the question "%s", and use only the following as the source of the answer: %s'
 PROMPTSYSTEM    = 'You are %s, and you respond in %s.'
-PERSONA         = 'an erudite scholar'
+PERSONA         = 'a sophmoric college student'
 LENGTH          = 'one paragraph'
 QUESTION        = 'What are the differences between library catalogs and discovery systems?'
 
@@ -26,13 +26,14 @@ from reader_libraries import carrel, searcher, citations, summarizer, elaborator
 
 # initialize, search, and get paragraphs
 carrel    = carrel( KEY, NAME )
-results   = searcher().search( carrel, QUERY, DEPTH )
+searcher  = searcher()
+results   = searcher.search( carrel, QUERY, DEPTH )
 paragraph = citations( results ).to_paragraph()
 
 # summarize
-prompt    = PROMPTSUMMARIZE % ( paragraph )
-system    = PROMPTSYSTEM % ( PERSONA, LENGTH )
-summary   = summarizer().summarize( LLM, prompt, system  )
+prompt  = PROMPTSUMMARIZE % ( paragraph )
+system  = PROMPTSYSTEM % ( PERSONA, LENGTH )
+summary = summarizer().summarize( LLM, prompt, system  )
 
 # elaborate
 prompt      = PROMPTELABORATE % ( QUESTION, paragraph )
@@ -40,5 +41,11 @@ system      = PROMPTSYSTEM % ( PERSONA, LENGTH )
 elaboration = elaborator().elaborate( LLM, prompt, system )
 
 # output and done
-print( elaboration[ 'response' ] )
+print( 'Journal - %s\n' % ( carrel.name ) )
+print( 'Query - %s\n' % ( searcher.query ) )
+print( 'Depth - %s\n' % ( searcher.depth ) )
+print( 'Sentences - %s\n' % ( paragraph ) )
+print( 'Summary - %s\n' % ( summary[ 'response' ] ) )
+print( 'Question - %s\n'% ( QUESTION ) )
+print( 'Answer - %s\n'% ( elaboration[ 'response' ] ) )
 exit()
